5e9ddffcbf4e6bacf16b6fca782df2633e177469
Andros.Evgen <andros.evgen@gmail.com>
2019-01-17 16:56:05 +0200

    Fist

--- conn_db.py
+++ conn_db.py
@@ -1,14 +1,23 @@
 import cx_Oracle
 import os, sys, json
+import conf
 
+j1 = json.loads(conf
+                .logdate)
+login = j1["login"]
+passwords = j1["passwords"]
+ip = j1["ip"]
+date_name = j1["date_name"]
+c_con = login + "/" + passwords + "@" + ip + "/" + date_name
 
-
-
-
-
-'''con = cx_Oracle.connect('login/passwords@ip/date_name')
-print (con.version)
+#con = cx_Oracle.connect('login/passwords@ip/date_name')
+con = cx_Oracle.connect(c_con)
+if (con):
+    print(
+    "Connection successful")
+    print(
+    con.version)
+else:
+    print(
+    "Connection not successful")
 con.close()
-'''
-
-
